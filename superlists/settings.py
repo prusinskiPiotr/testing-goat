69a5298fe29c84e5b028e3ded2bb523adde28656
Piotr <prusinski.pio@gmail.com>
2018-06-19 12:49:04 +0200

    Add logging to stderr

--- superlists/settings.py
+++ superlists/settings.py
@@ -141,3 +141,21 @@ EMAIL_HOST_USER = 'dravenx22@gmail.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
+
