f11f6e51658de5c95d936141495139cb861aca6b
Piotr <prusinski.pio@gmail.com>
2018-06-28 11:57:43 +0200

    Update fabfile to retrieve email from env file

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,6 +1,7 @@
 import random
 from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
+import os
 
 REPO_URL = 'https://github.com/prusinskiPiotr/testing-goat'
 
@@ -37,6 +38,8 @@ def _create_or_update_dotenv():
     if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices('abcdefghijklmnopqrstuvwxyz0123456789', k=50))
         append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+    email_password = os.environ['EMAIL_PASSWORD']
+    append('.env', f'EMAIL_PASSWORD={email_password}')
 
 
 def _update_static_files():
