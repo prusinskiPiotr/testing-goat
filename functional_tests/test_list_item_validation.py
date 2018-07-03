90006e5bb4d698073890c435f60a25a90670290e
Piotr <prusinski.pio@gmail.com>
2018-07-03 15:31:06 +0200

    Add url, placeholder view, and first-cut templates for my_lists

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -54,10 +54,10 @@ class ItemValidationTest(FunctionalTest):
         # Edith goes to home page and starts a new list
         self.browser.get(self.live_server_url)
         self.add_list_item('Buy wellies')
-        self.wait_for_row_in_list_table('1: Buy wellies')
 
         # She accidentally tries to enter a duplicate item
-        self.add_list_item('Buy wellies')
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         # She sees a helpful error message
         self.wait_for(lambda: self.assertEqual(
@@ -69,8 +69,8 @@ class ItemValidationTest(FunctionalTest):
         # Edith starts a list and causes a validation error:
         self.browser.get(self.live_server_url)
         self.add_list_item('Banter too thick')
-        self.wait_for_row_in_list_table('1: Banter too thick')
-        self.add_list_item('Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertTrue(
             self.get_error_element().is_displayed()
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -152,3 +152,9 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
+
+class MyListsTest(TestCase):
+
+    def test_my_lists_url_renders_my_lists_template(self):
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertTemplateUsed(response, 'my_lists.html')
